521f3bc6ab7911d5a4342a816515bad4594a16c0
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-06-13 18:47:35 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,4 +3,4 @@ git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 pip3 install -U -r re*/st*/optional-requirements.txt
-bash startup
+python3 -m pyUltroid
